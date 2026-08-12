8b93d5e4c12e0be127ebcbb2956b29be08b40c83
Tina-noori <tinanoori725@gmail.com>
2026-08-13 04:21:48 +0430

    Add hangman words

---
+++ hangman_words.py
@@ -0,0 +1,5 @@
+word_list=[
+           "Apple", "Book","Cat","Doll","Elephant","Fox","dog","car","table",
+           "board","hot","cold","skate","horse","tennis","bike","van","bus","pen",
+            "pig","mop","bed","pizza","grapes","pears","art","music","mom","clock",
+            ]
